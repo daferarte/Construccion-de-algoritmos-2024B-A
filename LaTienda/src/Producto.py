fd11ca10037b85eb35cd21cfea8a1e2b53d0cd0b
daferarte <daferarte@gmail.com>
2024-09-30 09:38:54 -0500

    Creando clase la tienda

---
+++ LaTienda/src/Producto.py
@@ -0,0 +1,45 @@
+__author__ = "Daniel Fernando Arteaga Fajardo"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "daniel.arteagafajar@campusucc.edu.co"
+
+import constantes
+
+class Producto:
+    
+    """----------------------------------------------------------------
+    # Constantes
+    ----------------------------------------------------------------"""
+    
+    IVA_PAPELERIA = 0.16
+    IVA_SUPERMERCADO = 0.04
+    IVA_FARMACIA = 0.12
+    
+    """----------------------------------------------------------------
+    # Constructor
+    ----------------------------------------------------------------"""
+    
+    def __init__(self, nValorUnitario:float, nSubsidiado:bool, nCalidad):
+        
+        """----------------------------------------------------------------
+        # Atributos
+        ----------------------------------------------------------------"""
+        
+        self.__valorUnitario = nValorUnitario
+        self.__subsidiado = nSubsidiado
+        self.__calidad = nCalidad
+        
+        
+        
+        """----------------------------------------------------------------
+        # self.__subsidiado = True
+        # self.__subsidiado = False
+        ----------------------------------------------------------------"""
+        
+        
+    def CalcularPrecioSupermercador(self):
+        
+        # self.__precio = self.__precio + (self.__precio * self.IVA_SUPERMERCADO)
+        # self.__precio = self.__precio + (self.__precio * constantes.IVA_SUPERMERCADO)
+        
+        self.__precio += (self.__precio * constantes.IVA_SUPERMERCADO)
---
+++ LaTienda/src/constantes.py
@@ -0,0 +1,12 @@
+__author__ = "Daniel Fernando Arteaga Fajardo"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "daniel.arteagafajar@campusucc.edu.co"
+
+"""----------------------------------------------------------------
+# Constantes
+----------------------------------------------------------------"""
+
+IVA_PAPELERIA = 0.16
+IVA_SUPERMERCADO = 0.04
+IVA_FARMACIA = 0.12
