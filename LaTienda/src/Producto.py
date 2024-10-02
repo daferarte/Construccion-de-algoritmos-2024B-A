4518173f715ff7a61596c00f9ff442b58686e694
daferarte <daferarte@gmail.com>
2024-10-02 08:35:45 -0500

    Ajuste en productos

--- LaTienda/src/Producto.py
+++ LaTienda/src/Producto.py
@@ -19,24 +19,66 @@ class Producto:
     # Constructor
     ----------------------------------------------------------------"""
     
-    def __init__(self, nValorUnitario:float, nSubsidiado:bool, nCalidad):
+    def __init__(self,nombre, tipo, nValorUnitario:float, cantidadBodega, nSubsidiado:bool, nCalidad):
         
         """----------------------------------------------------------------
         # Atributos
         ----------------------------------------------------------------"""
-        
+        self.__nombre=nombre
+        self.__tipo=tipo
         self.__valorUnitario = nValorUnitario
+        self.__cantidadBodega = cantidadBodega
         self.__subsidiado = nSubsidiado
         self.__calidad = nCalidad
-        self.___tipoVenta = Tipo.FARMACIA
+        self.__cantidadMinima = 0
+        self.__cantidadUnidadesVendidas = 0
         
-        
-        """----------------------------------------------------------------
-        # self.__subsidiado = True
-        # self.__subsidiado = False
-        ----------------------------------------------------------------"""
-        
-        
+    __method__='DarNombre'
+    __params__='Ninguno'
+    __returns__='Nombre'
+    __descriptions__='Este metodo retorna el nombre del producto'
+    def DarNombre(self):
+        return self.__nombre
+    
+    __method__='DarTipo'
+    __params__='Ninguno'
+    __returns__='tipo'
+    __descriptions__='Este metodo retorna el tipo del producto'
+    def DarTipo(self):
+        return self.__tipo
+    
+    __method__='DarValorUnitario'
+    __params__='Ninguno'
+    __returns__='valorUnitario'
+    __descriptions__='Este metodo retorna el valorUnitario del producto'
+    def DarValorUnitario(self):
+        return self.__valorUnitario
+    
+    __method__='DarCantidadBodega'
+    __params__='Ninguno'
+    __returns__='cantidadBodega'
+    __descriptions__='Este metodo retorna la cantidadBodega del producto'
+    def DarCantidadBodega(self):
+        return self.__cantidadBodega
+    
+    __method__='DarCantidadMinima'
+    __params__='Ninguno'
+    __returns__='cantidadMinima'
+    __descriptions__='Este metodo retorna la cantidadMinima del producto'
+    def DarCantidadMinima(self):
+        return self.__cantidadMinima
+    
+    __method__='DarCantidadUnidadesVendidas'
+    __params__='Ninguno'
+    __returns__='cantidadUnidadesVendidas'
+    __descriptions__='Este metodo retorna la cantidadUnidadesVendidas del producto'
+    def DarCantidadUnidadesVendidas(self):
+        return self.__cantidadUnidadesVendidas
+    
+    __method__='CalcularPrecioSupermercador'
+    __params__='Ninguno'
+    __returns__='precioSupermercado'
+    __descriptions__='Este metodo calcula el precio del supermercado'
     def CalcularPrecioSupermercador(self):
         
         # self.__precio = self.__precio + (self.__precio * self.IVA_SUPERMERCADO)
